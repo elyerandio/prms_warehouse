6fbded6dd2135b9af6dea6aab8008962cbab51a8
Ely Erandio <elyerandio@gmail.com>
2023-06-05 10:32:46 +0800

    dp_upload.go modified.

--- dp_upload.go
+++ dp_upload.go
@@ -128,220 +128,220 @@ type Invoice struct {
 }
 
 type Vendor struct {
-	ACTIV string    `db:"ACTIV"`
-	CMPNO int       `db:"CMPNO"`
-	PLTNO int       `db:"PLTNO"`
-	VNDNO int       `db:"VNDNO"`
-	CRPVD int       `db:"CRPVD"`
-	VNAME string    `db:"VNAME"`
-	VADD1 string    `db:"VADD1"`
-	VADD2 string    `db:"VADD2"`
-	VADDX string    `db:"VADDX"`
-	VADD3 string    `db:"VADD3"`
-	VSTAT string    `db:"VSTAT"`
-	VZIPC string    `db:"VZIPC"`
-	PHONE float64   `db:"PHONE"`
-	VPHOA string    `db:"VPHOA"`
-	VTLXN string    `db:"VTLXN"`
-	VFAXN string    `db:"VFAXN"`
-	VTEMP string    `db:"VTEMP"`
-	ALPHA string    `db:"ALPHA"`
-	BNKN1 int       `db:"BNKN1"`
-	VAPGL float64   `db:"VAPGL"`
-	VDSGL float64   `db:"VDSGL"`
-	VDPGL float64   `db:"VDPGL"`
-	VMLDT time.Time `db:"VMLDT"`
-	TOTDU float64   `db:"TOTDU"`
-	FTOTD float64   `db:"FTOTD"`
-	MTDPY float64   `db:"MTDPY"`
-	FMTDP float64   `db:"FMTDP"`
-	PD2PY float64   `db:"PD2PY"`
-	FP2PY float64   `db:"FP2PY"`
-	YTDPY float64   `db:"YTDPY"`
-	FYTDP float64   `db:"FYTDP"`
-	PYDPY float64   `db:"PYDPY"`
-	FPYPY float64   `db:"FPYPY"`
-	MTDDS float64   `db:"MTDDS"`
-	FMTDD float64   `db:"FMTDD"`
-	PD2DS float64   `db:"PD2DS"`
-	FP2DS float64   `db:"FP2DS"`
-	YTDDS float64   `db:"YTDDS"`
-	FYTDD float64   `db:"FYTDD"`
-	PYDDS float64   `db:"PYDDS"`
-	FPYDS float64   `db:"FPYDS"`
-	MTDLS float64   `db:"MTDLS"`
-	FMTLS float64   `db:"FMTLS"`
-	PD2LS float64   `db:"PD2LS"`
-	FP2LS float64   `db:"FP2LS"`
-	YTDLS float64   `db:"YTDLS"`
-	FYTLS float64   `db:"FYTLS"`
-	PYDLS float64   `db:"PYDLS"`
-	FPYLS float64   `db:"FPYLS"`
-	MTDPC float64   `db:"MTDPC"`
-	FMTPC float64   `db:"FMTPC"`
-	PD2PC float64   `db:"PD2PC"`
-	FP2PC float64   `db:"FP2PC"`
-	YTDPC float64   `db:"YTDPC"`
-	FYTPC float64   `db:"FYTPC"`
-	PYDPC float64   `db:"PYDPC"`
-	FPYPC float64   `db:"FPYPC"`
-	VCMNT string    `db:"VCMNT"`
-	VRPCT float64   `db:"VRPCT"`
-	F1099 string    `db:"F1099"`
-	FEDID string    `db:"FEDID"`
-	YTD99 float64   `db:"YTD99"`
-	NXT99 float64   `db:"NXT99"`
-	LSTCY int       `db:"LSTCY"`
-	VDAYS int       `db:"VDAYS"`
-	NTMDY int       `db:"NTMDY"`
-	VCRCD string    `db:"VCRCD"`
-	DISCP float64   `db:"DISCP"`
-	DISTD int       `db:"DISTD"`
-	VPHLD string    `db:"VPHLD"`
-	VFDSC string    `db:"VFDSC"`
-	VSDSC string    `db:"VSDSC"`
-	VDNPY string    `db:"VDNPY"`
-	VCONT string    `db:"VCONT"`
-	VCAT1 string    `db:"VCAT1"`
-	VCAT2 string    `db:"VCAT2"`
-	OPDIV string    `db:"OPDIV"`
-	VPNXT string    `db:"VPNXT"`
-	RMAD1 string    `db:"RMAD1"`
-	RMAD2 string    `db:"RMAD2"`
-	RMADX string    `db:"RMADX"`
-	RMAD3 string    `db:"RMAD3"`
-	RMSTA string    `db:"RMSTA"`
-	RMZIP string    `db:"RMZIP"`
-	VST01 float64   `db:"VST01"`
-	VST02 float64   `db:"VST02"`
-	VST03 float64   `db:"VST03"`
-	VST04 float64   `db:"VST04"`
-	VST05 float64   `db:"VST05"`
-	VST06 float64   `db:"VST06"`
-	VST07 float64   `db:"VST07"`
-	VST08 float64   `db:"VST08"`
-	VST09 float64   `db:"VST09"`
-	VST10 float64   `db:"VST10"`
-	VST11 float64   `db:"VST11"`
-	VST12 float64   `db:"VST12"`
-	VST13 float64   `db:"VST13"`
-	FVT01 float64   `db:"FVT01"`
-	FVT02 float64   `db:"FVT02"`
-	FVT03 float64   `db:"FVT03"`
-	FVT04 float64   `db:"FVT04"`
-	FVT05 float64   `db:"FVT05"`
-	FVT06 float64   `db:"FVT06"`
-	FVT07 float64   `db:"FVT07"`
-	FVT08 float64   `db:"FVT08"`
-	FVT09 float64   `db:"FVT09"`
-	FVT10 float64   `db:"FVT10"`
-	FVT11 float64   `db:"FVT11"`
-	FVT12 float64   `db:"FVT12"`
-	FVT13 float64   `db:"FVT13"`
-	VSL01 float64   `db:"VSL01"`
-	VSL02 float64   `db:"VSL02"`
-	VSL03 float64   `db:"VSL03"`
-	VSL04 float64   `db:"VSL04"`
-	VSL05 float64   `db:"VSL05"`
-	VSL06 float64   `db:"VSL06"`
-	VSL07 float64   `db:"VSL07"`
-	VSL08 float64   `db:"VSL08"`
-	VSL09 float64   `db:"VSL09"`
-	VSL10 float64   `db:"VSL10"`
-	VSL11 float64   `db:"VSL11"`
-	VSL12 float64   `db:"VSL12"`
-	VSL13 float64   `db:"VSL13"`
-	FVL01 float64   `db:"FVL01"`
-	FVL02 float64   `db:"FVL02"`
-	FVL03 float64   `db:"FVL03"`
-	FVL04 float64   `db:"FVL04"`
-	FVL05 float64   `db:"FVL05"`
-	FVL06 float64   `db:"FVL06"`
-	FVL07 float64   `db:"FVL07"`
-	FVL08 float64   `db:"FVL08"`
-	FVL09 float64   `db:"FVL09"`
-	FVL10 float64   `db:"FVL10"`
-	FVL11 float64   `db:"FVL11"`
-	FVL12 float64   `db:"FVL12"`
-	FVL13 float64   `db:"FVL13"`
-	ONORV float64   `db:"ONORV"`
-	FONOV float64   `db:"FONOV"`
-	VMMIN float64   `db:"VMMIN"`
-	VMMAX float64   `db:"VMMAX"`
-	VMAVO float64   `db:"VMAVO"`
-	VMIMW float64   `db:"VMIMW"`
-	VPOHL string    `db:"VPOHL"`
-	VMOUT string    `db:"VMOUT"`
-	VMVIA string    `db:"VMVIA"`
-	VMCAR string    `db:"VMCAR"`
-	VMFRT string    `db:"VMFRT"`
-	VMFTD string    `db:"VMFTD"`
-	VRCBY float64   `db:"VRCBY"`
-	VRCBM float64   `db:"VRCBM"`
-	VRCBP float64   `db:"VRCBP"`
-	VRCAY float64   `db:"VRCAY"`
-	FRCAY float64   `db:"FRCAY"`
-	VRCAM float64   `db:"VRCAM"`
-	FRCAM float64   `db:"FRCAM"`
-	VRCAP float64   `db:"VRCAP"`
-	FRCAP float64   `db:"FRCAP"`
-	VJCBY float64   `db:"VJCBY"`
-	VJCBM float64   `db:"VJCBM"`
-	VJCBP float64   `db:"VJCBP"`
-	VMSCM float64   `db:"VMSCM"`
-	VMSCY float64   `db:"VMSCY"`
-	VMRVM float64   `db:"VMRVM"`
-	VMRVY float64   `db:"VMRVY"`
-	VMRWM float64   `db:"VMRWM"`
-	VMRWY float64   `db:"VMRWY"`
-	VNESY int       `db:"VNESY"`
-	VNESM int       `db:"VNESM"`
-	VNESP int       `db:"VNESP"`
-	VNLSY int       `db:"VNLSY"`
-	VNLSM int       `db:"VNLSM"`
-	VNLSP int       `db:"VNLSP"`
-	VOTSY int       `db:"VOTSY"`
-	VOTSM int       `db:"VOTSM"`
-	VOTSP int       `db:"VOTSP"`
-	VPOCT string    `db:"VPOCT"`
-	VSMCT string    `db:"VSMCT"`
-	VMLPD int       `db:"VMLPD"`
-	VTXB1 string    `db:"VTXB1"`
-	VTXB2 string    `db:"VTXB2"`
-	VTXB3 string    `db:"VTXB3"`
-	VTAX1 string    `db:"VTAX1"`
-	VTAX2 string    `db:"VTAX2"`
-	VTAX3 string    `db:"VTAX3"`
-	VTXL1 string    `db:"VTXL1"`
-	VTXL2 string    `db:"VTXL2"`
-	VTXL3 string    `db:"VTXL3"`
-	V1099 int       `db:"V1099"`
-	VDDGL float64   `db:"VDDGL"`
-	VDDCM string    `db:"VDDCM"`
-	VDDST string    `db:"VDDST"`
-	VVRSN string    `db:"VVRSN"`
-	VIRSN string    `db:"VIRSN"`
-	VMSSD float64   `db:"VMSSD"`
-	VACGL float64   `db:"VACGL"`
-	VMPYC string    `db:"VMPYC"`
-	VMAPR string    `db:"VMAPR"`
-	VMPEN float64   `db:"VMPEN"`
-	VMFPN float64   `db:"VMFPN"`
-	VMTXP string    `db:"VMTXP"`
-	WBRCD string    `db:"WBRCD"`
-	VMEDI string    `db:"VMEDI"`
-	VMED1 string    `db:"VMED1"`
-	VMED2 string    `db:"VMED2"`
-	VMED3 string    `db:"VMED3"`
-	VMED4 float64   `db:"VMED4"`
-	VMED5 float64   `db:"VMED5"`
-	VMED6 float64   `db:"VMED6"`
-	VMDPD string    `db:"VMDPD"`
-	VMVSC string    `db:"VMVSC"`
-	VMVSE string    `db:"VMVSE"`
-	VMCTY string    `db:"VMCTY"`
-	VMSEC string    `db:"VMSEC"`
-	VMLNG string    `db:"VMLNG"`
+	ACTIV string    `db:"activ"`
+	CMPNO int       `db:"cmpno"`
+	PLTNO int       `db:"pltno"`
+	VNDNO int       `db:"vndno"`
+	CRPVD int       `db:"crpvd"`
+	VNAME string    `db:"vname"`
+	VADD1 string    `db:"vadd1"`
+	VADD2 string    `db:"vadd2"`
+	VADDX string    `db:"vaddx"`
+	VADD3 string    `db:"vadd3"`
+	VSTAT string    `db:"vstat"`
+	VZIPC string    `db:"vzipc"`
+	PHONE float64   `db:"phone"`
+	VPHOA string    `db:"vphoa"`
+	VTLXN string    `db:"vtlxn"`
+	VFAXN string    `db:"vfaxn"`
+	VTEMP string    `db:"vtemp"`
+	ALPHA string    `db:"alpha"`
+	BNKN1 int       `db:"bnkn1"`
+	VAPGL float64   `db:"vapgl"`
+	VDSGL float64   `db:"vdsgl"`
+	VDPGL float64   `db:"vdpgl"`
+	VMLDT time.Time `db:"vmldt"`
+	TOTDU float64   `db:"totdu"`
+	FTOTD float64   `db:"ftotd"`
+	MTDPY float64   `db:"mtdpy"`
+	FMTDP float64   `db:"fmtdp"`
+	PD2PY float64   `db:"pd2py"`
+	FP2PY float64   `db:"fp2py"`
+	YTDPY float64   `db:"ytdpy"`
+	FYTDP float64   `db:"fytdp"`
+	PYDPY float64   `db:"pydpy"`
+	FPYPY float64   `db:"fpypy"`
+	MTDDS float64   `db:"mtdds"`
+	FMTDD float64   `db:"fmtdd"`
+	PD2DS float64   `db:"pd2ds"`
+	FP2DS float64   `db:"fp2ds"`
+	YTDDS float64   `db:"ytdds"`
+	FYTDD float64   `db:"fytdd"`
+	PYDDS float64   `db:"pydds"`
+	FPYDS float64   `db:"fpyds"`
+	MTDLS float64   `db:"mtdls"`
+	FMTLS float64   `db:"fmtls"`
+	PD2LS float64   `db:"pd2ls"`
+	FP2LS float64   `db:"fp2ls"`
+	YTDLS float64   `db:"ytdls"`
+	FYTLS float64   `db:"fytls"`
+	PYDLS float64   `db:"pydls"`
+	FPYLS float64   `db:"fpyls"`
+	MTDPC float64   `db:"mtdpc"`
+	FMTPC float64   `db:"fmtpc"`
+	PD2PC float64   `db:"pd2pc"`
+	FP2PC float64   `db:"fp2pc"`
+	YTDPC float64   `db:"ytdpc"`
+	FYTPC float64   `db:"fytpc"`
+	PYDPC float64   `db:"pydpc"`
+	FPYPC float64   `db:"fpypc"`
+	VCMNT string    `db:"vcmnt"`
+	VRPCT float64   `db:"vrpct"`
+	F1099 string    `db:"f1099"`
+	FEDID string    `db:"fedid"`
+	YTD99 float64   `db:"ytd99"`
+	NXT99 float64   `db:"nxt99"`
+	LSTCY int       `db:"lstcy"`
+	VDAYS int       `db:"vdays"`
+	NTMDY int       `db:"ntmdy"`
+	VCRCD string    `db:"vcrcd"`
+	DISCP float64   `db:"discp"`
+	DISTD int       `db:"distd"`
+	VPHLD string    `db:"vphld"`
+	VFDSC string    `db:"vfdsc"`
+	VSDSC string    `db:"vsdsc"`
+	VDNPY string    `db:"vdnpy"`
+	VCONT string    `db:"vcont"`
+	VCAT1 string    `db:"vcat1"`
+	VCAT2 string    `db:"vcat2"`
+	OPDIV string    `db:"opdiv"`
+	VPNXT string    `db:"vpnxt"`
+	RMAD1 string    `db:"rmad1"`
+	RMAD2 string    `db:"rmad2"`
+	RMADX string    `db:"rmadx"`
+	RMAD3 string    `db:"rmad3"`
+	RMSTA string    `db:"rmsta"`
+	RMZIP string    `db:"rmzip"`
+	VST01 float64   `db:"vst01"`
+	VST02 float64   `db:"vst02"`
+	VST03 float64   `db:"vst03"`
+	VST04 float64   `db:"vst04"`
+	VST05 float64   `db:"vst05"`
+	VST06 float64   `db:"vst06"`
+	VST07 float64   `db:"vst07"`
+	VST08 float64   `db:"vst08"`
+	VST09 float64   `db:"vst09"`
+	VST10 float64   `db:"vst10"`
+	VST11 float64   `db:"vst11"`
+	VST12 float64   `db:"vst12"`
+	VST13 float64   `db:"vst13"`
+	FVT01 float64   `db:"fvt01"`
+	FVT02 float64   `db:"fvt02"`
+	FVT03 float64   `db:"fvt03"`
+	FVT04 float64   `db:"fvt04"`
+	FVT05 float64   `db:"fvt05"`
+	FVT06 float64   `db:"fvt06"`
+	FVT07 float64   `db:"fvt07"`
+	FVT08 float64   `db:"fvt08"`
+	FVT09 float64   `db:"fvt09"`
+	FVT10 float64   `db:"fvt10"`
+	FVT11 float64   `db:"fvt11"`
+	FVT12 float64   `db:"fvt12"`
+	FVT13 float64   `db:"fvt13"`
+	VSL01 float64   `db:"vsl01"`
+	VSL02 float64   `db:"vsl02"`
+	VSL03 float64   `db:"vsl03"`
+	VSL04 float64   `db:"vsl04"`
+	VSL05 float64   `db:"vsl05"`
+	VSL06 float64   `db:"vsl06"`
+	VSL07 float64   `db:"vsl07"`
+	VSL08 float64   `db:"vsl08"`
+	VSL09 float64   `db:"vsl09"`
+	VSL10 float64   `db:"vsl10"`
+	VSL11 float64   `db:"vsl11"`
+	VSL12 float64   `db:"vsl12"`
+	VSL13 float64   `db:"vsl13"`
+	FVL01 float64   `db:"fvl01"`
+	FVL02 float64   `db:"fvl02"`
+	FVL03 float64   `db:"fvl03"`
+	FVL04 float64   `db:"fvl04"`
+	FVL05 float64   `db:"fvl05"`
+	FVL06 float64   `db:"fvl06"`
+	FVL07 float64   `db:"fvl07"`
+	FVL08 float64   `db:"fvl08"`
+	FVL09 float64   `db:"fvl09"`
+	FVL10 float64   `db:"fvl10"`
+	FVL11 float64   `db:"fvl11"`
+	FVL12 float64   `db:"fvl12"`
+	FVL13 float64   `db:"fvl13"`
+	ONORV float64   `db:"onorv"`
+	FONOV float64   `db:"fonov"`
+	VMMIN float64   `db:"vmmin"`
+	VMMAX float64   `db:"vmmax"`
+	VMAVO float64   `db:"vmavo"`
+	VMIMW float64   `db:"vmimw"`
+	VPOHL string    `db:"vpohl"`
+	VMOUT string    `db:"vmout"`
+	VMVIA string    `db:"vmvia"`
+	VMCAR string    `db:"vmcar"`
+	VMFRT string    `db:"vmfrt"`
+	VMFTD string    `db:"vmftd"`
+	VRCBY float64   `db:"vrcby"`
+	VRCBM float64   `db:"vrcbm"`
+	VRCBP float64   `db:"vrcbp"`
+	VRCAY float64   `db:"vrcay"`
+	FRCAY float64   `db:"frcay"`
+	VRCAM float64   `db:"vrcam"`
+	FRCAM float64   `db:"frcam"`
+	VRCAP float64   `db:"vrcap"`
+	FRCAP float64   `db:"frcap"`
+	VJCBY float64   `db:"vjcby"`
+	VJCBM float64   `db:"vjcbm"`
+	VJCBP float64   `db:"vjcbp"`
+	VMSCM float64   `db:"vmscm"`
+	VMSCY float64   `db:"vmscy"`
+	VMRVM float64   `db:"vmrvm"`
+	VMRVY float64   `db:"vmrvy"`
+	VMRWM float64   `db:"vmrwm"`
+	VMRWY float64   `db:"vmrwy"`
+	VNESY int       `db:"vnesy"`
+	VNESM int       `db:"vnesm"`
+	VNESP int       `db:"vnesp"`
+	VNLSY int       `db:"vnlsy"`
+	VNLSM int       `db:"vnlsm"`
+	VNLSP int       `db:"vnlsp"`
+	VOTSY int       `db:"votsy"`
+	VOTSM int       `db:"votsm"`
+	VOTSP int       `db:"votsp"`
+	VPOCT string    `db:"vpoct"`
+	VSMCT string    `db:"vsmct"`
+	VMLPD int       `db:"vmlpd"`
+	VTXB1 string    `db:"vtxb1"`
+	VTXB2 string    `db:"vtxb2"`
+	VTXB3 string    `db:"vtxb3"`
+	VTAX1 string    `db:"vtax1"`
+	VTAX2 string    `db:"vtax2"`
+	VTAX3 string    `db:"vtax3"`
+	VTXL1 string    `db:"vtxl1"`
+	VTXL2 string    `db:"vtxl2"`
+	VTXL3 string    `db:"vtxl3"`
+	V1099 int       `db:"v1099"`
+	VDDGL float64   `db:"vddgl"`
+	VDDCM string    `db:"vddcm"`
+	VDDST string    `db:"vddst"`
+	VVRSN string    `db:"vvrsn"`
+	VIRSN string    `db:"virsn"`
+	VMSSD float64   `db:"vmssd"`
+	VACGL float64   `db:"vacgl"`
+	VMPYC string    `db:"vmpyc"`
+	VMAPR string    `db:"vmapr"`
+	VMPEN float64   `db:"vmpen"`
+	VMFPN float64   `db:"vmfpn"`
+	VMTXP string    `db:"vmtxp"`
+	WBRCD string    `db:"wbrcd"`
+	VMEDI string    `db:"vmedi"`
+	VMED1 string    `db:"vmed1"`
+	VMED2 string    `db:"vmed2"`
+	VMED3 string    `db:"vmed3"`
+	VMED4 float64   `db:"vmed4"`
+	VMED5 float64   `db:"vmed5"`
+	VMED6 float64   `db:"vmed6"`
+	VMDPD string    `db:"vmdpd"`
+	VMVSC string    `db:"vmvsc"`
+	VMVSE string    `db:"vmvse"`
+	VMCTY string    `db:"vmcty"`
+	VMSEC string    `db:"vmsec"`
+	VMLNG string    `db:"vmlng"`
 }
 
 type Bank struct {
@@ -476,6 +476,10 @@ func main() {
 	fmt.Printf("Error count : %d\n", errorCount)
 	fmt.Printf("Uploaded count : %d\n", saveCount)
 	fmt.Print("\nPress ENTER to continue...")
+	fmt.Fprintf(errorFile, "\nError count : %s\n", humanize.Comma(int64(errorCount)))
+	if runType == "Final" {
+		fmt.Fprintf(errorFile, "\nUploaded count : %s\n", humanize.Comma(int64(saveCount)))
+	}
 	fmt.Scanln()
 }
 
@@ -483,7 +487,7 @@ func readIni() {
 	var err error
 
 	// dp_upload.ini is in the Linux server 172.20.0.39
-	// cpRemoteIni() copies the remote init file to local pc to be read by the program
+	// cpRemoteIni() copies the remote ini file to local pc to be read by the program
 	iniFilename := cpRemoteIni()
 	defer os.Remove(iniFilename)
 
@@ -779,8 +783,7 @@ func uploadCSV(filename string) {
 
 		// get the column data to upload
 		vendorNum = strings.TrimSpace(record[0])
-		// remove double quote(") in invoiceNum
-		invoiceNum = strings.ReplaceAll(strings.TrimSpace(record[1]), "\"", "")
+		invoiceNum = strings.TrimSpace(record[1])
 		strAmount = strings.TrimSpace(record[2])
 		customerNum = strings.TrimSpace(record[3])
 		invoiceDate = strings.TrimSpace(record[4])
@@ -822,6 +825,10 @@ func uploadCSV(filename string) {
 		if runType == "Final" && !errorFound {
 			saveInvoice()
 		}
+
+		if !errorFound {
+			printRemark()
+		}
 	}
 }
 
@@ -831,13 +838,16 @@ func getVendor(vendorNum string) (Vendor, error) {
 	fieldsCsv := fieldsCSV(fields)
 	// fieldsCSVColons := fieldsCSVColons(fields)
 
-	selectStmt := fmt.Sprintf("SELECT %s FROM RMSMDFL#.MSVMP100 WHERE vndno=?", fieldsCsv)
-	err := dbOdbc.QueryRowx(selectStmt, vendorNum).StructScan(&vendor)
+	selectStmt := fmt.Sprintf("SELECT %s FROM MSVMP100 WHERE vndno=$1", fieldsCsv)
+	// err := dbOdbc.QueryRowx(selectStmt, vendorNum).StructScan(&vendor)
+	err := dbPostgre.QueryRowx(selectStmt, vendorNum).StructScan(&vendor)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			return vendor, err
 		}
 
+		fmt.Println()
+		fmt.Println(selectStmt)
 		panic(err)
 	}
 
@@ -1040,6 +1050,28 @@ func printError() {
 	fmt.Fprintf(errorFile, "%8.8s", invoiceDate)
 	fmt.Fprintf(errorFile, "%16.16s", " ")
 	fmt.Fprintf(errorFile, "%s\n", errorMsg)
+}
+
+func printRemark() {
+	fmt.Fprintf(errorFile, "%05d", lineCount)
+	fmt.Fprintf(errorFile, "%5.5s", " ")
+	fmt.Fprintf(errorFile, "%6.6s", vendorNum)
+	fmt.Fprintf(errorFile, "%8.8s", " ")
+	fmt.Fprintf(errorFile, "%-20.20s", invoiceNum)
+	fmt.Fprintf(errorFile, "%9.9s", " ")
+
+	amount, _ := strconv.ParseFloat(strAmount, 64)
+	fmt.Fprintf(errorFile, "%14.14s", humanize.FormatFloat("#,###.##", amount))
+	fmt.Fprintf(errorFile, "%14.14s", " ")
+	fmt.Fprintf(errorFile, "%6.6s", customerNum)
+	fmt.Fprintf(errorFile, "%12.12s", " ")
+	fmt.Fprintf(errorFile, "%8.8s", invoiceDate)
+	fmt.Fprintf(errorFile, "%16.16s", " ")
+	if runType == "Final" {
+		fmt.Fprintf(errorFile, "Uploaded\n")
+	} else {
+		fmt.Fprintf(errorFile, "Passed\n")
+	}
 }
 
 // vDuplicateInDB - check if there is a duplicate in the database and in the input file
